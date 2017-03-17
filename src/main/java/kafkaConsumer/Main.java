3150a11c5ada11ba76c65c0ac21d7c92d334063e
Muhammad Anis Uddin Nasir <Anis@muhammads-mbp.dhcp.hakozaki.ibm.com>
2017-03-17 14:13:06 +0900

    added code

--- src/main/java/kafkaConsumer/Main.java
+++ src/main/java/kafkaConsumer/Main.java
@@ -1,49 +1,41 @@
 package kafkaConsumer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.function.Consumer;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
+
 public class Main {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
+		String topicName = args[0];
+		Properties configProperties = new Properties();
+		configProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+		configProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+		configProperties.put(ConsumerConfig.GROUP_ID_CONFIG, topicName);
+		configProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, "simple");
 
-		// Check arguments length value
-		if(args.length == 0){
-			System.out.println("test");
-			return;
-		}
-
-		//Assign topicName to string variable
-		String topicName = args[0].toString();
-
-		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:9092");
-		props.put("group.id", "test");
-		props.put("enable.auto.commit", "true");
-		props.put("auto.commit.interval.ms", "1000");
-		props.put("session.timeout.ms", "30000");
-		props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");  
-		props.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("partition.assignment.strategy", "range");
-
-		KafkaConsumer<String, String> consumer = new KafkaConsumer<String,String>(props);
-		consumer.subscribe(topicName);
-
+		//Figure out where to start processing messages from
+		KafkaConsumer kafkaConsumer = new KafkaConsumer<String, String>(configProperties);
+		kafkaConsumer.subscribe(topicName);
+		//Start processing messages
 		try {
 			while (true) {
-				Map<String, ConsumerRecords<String, String>> records = consumer.poll(1000);
+				Map records = kafkaConsumer.poll(100);
 				System.out.println(records);
 			}
-		} finally {
-			consumer.close();
+		}catch(Exception ex){
+			System.out.println("Exception caught " + ex.getMessage());
+		}finally{
+			kafkaConsumer.close();
+			System.out.println("After closing KafkaConsumer");
 		}
-	}
 
+	}
 }
