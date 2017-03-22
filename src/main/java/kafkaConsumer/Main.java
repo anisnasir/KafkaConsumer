69dc9c94d9df4341f9599ec42692c72856cf7141
Muhammad Anis Uddin Nasir <Anis@muhammads-mbp.dhcp.hakozaki.ibm.com>
2017-03-22 11:26:26 +0900

    Added code

--- src/main/java/kafkaConsumer/Main.java
+++ src/main/java/kafkaConsumer/Main.java
@@ -16,34 +16,28 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		String topicName = args[0];
 		Properties configProperties = new Properties();
-		configProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "9.116.35.208:2181");
+		configProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "9.116.35.208:9092");
 		configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 		configProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 		configProperties.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
 		configProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, "simple");
-		configProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "smallest");
-		configProperties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY, "range");
+		configProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
 		//Figure out where to start processing messages from
 		KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(configProperties);
-		kafkaConsumer.subscribe(topicName);
+		kafkaConsumer.subscribe(Arrays.asList(topicName));
 		//Start processing messages
 		try {
-			int count =0;
-
 			while (true) {
-				Map<String, ConsumerRecords<String, String>> records = kafkaConsumer.poll(100);
-				System.out.println(records);
-				if(count++ == 10)
-					break;
+				ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
+				for(ConsumerRecord<String, String> record: records)
+					System.out.println(record.key() + " " + record.value());
 			}
-
 		}catch(Exception ex){
 			System.out.println("Exception caught " + ex.getMessage());
 		}finally{
 			kafkaConsumer.close();
 			System.out.println("After closing KafkaConsumer");
 		}
-
 	}
 }
