a2b8059dd284764aab91921e83c16e9f1014cbeb
Muhammad Anis Uddin Nasir <Anis@muhammads-mbp.dhcp.hakozaki.ibm.com>
2017-03-22 10:03:11 +0900

    added code

--- src/main/java/kafkaConsumer/Main.java
+++ src/main/java/kafkaConsumer/Main.java
@@ -15,29 +15,35 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		String topicName = args[0];
-		Properties configProperties = new Properties();
-		configProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "9.116.35.208:9092");
-		configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-		configProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-		configProperties.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
-		configProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, "simple");
-		configProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		configProperties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY, "range");
+		Properties props = new Properties();
+		//configProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "9.116.35.208:9092");
+		//configProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+		//configProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+		//configProperties.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+		//configProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, "simple");
+		//configProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		//configProperties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY, "range");
+
+		props.put("metadata.broker.list", "9.116.35.208:9092");
+		props.put("group.id", UUID.randomUUID().toString());
+		props.put("session.timeout.ms", "1000");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "10000");
 
 		//Figure out where to start processing messages from
-		KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(configProperties);
+		KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(props);
 		kafkaConsumer.subscribe(topicName);
 		//Start processing messages
 		try {
 			int count =0;
-			
+
 			while (true) {
 				Map<String, ConsumerRecords<String, String>> records = kafkaConsumer.poll(100);
 				System.out.println(records);
 				if(count++ == 10)
 					break;
 			}
-					
+
 		}catch(Exception ex){
 			System.out.println("Exception caught " + ex.getMessage());
 		}finally{
